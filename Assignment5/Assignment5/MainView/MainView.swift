bc3a471386247ced2b15cdfd5e28e092d1861bb9
kat2n <katsumata.coffee.150yen@gmail.com>
2019-04-30 15:38:33 -0700

    4. As a user, I can enter a tip percentage

--- Assignment5/Assignment5/MainView/MainView.swift
+++ Assignment5/Assignment5/MainView/MainView.swift
@@ -2,10 +2,14 @@ import UIKit
 
 class MainView: UIView {
 
+  let defaultTipPercentage = 15
+
   var ctrl: MainViewController!
 
   var billAmountLabel: UILabel!
   var billAmountField: UITextField!
+  var tipPercentageLabel: UILabel!
+  var tipPercentageField: UITextField!
   var calculateTipButton: UIButton!
   var tipAmountLabel: UILabel!
 
@@ -19,6 +23,8 @@ class MainView: UIView {
     self.ctrl = ctrl
     self.billAmountLabel = createBillAmountLabel()
     self.billAmountField = createBillAmountField()
+    self.tipPercentageLabel = createTipPercentageLabel()
+    self.tipPercentageField = createTipPercentageField()
     self.calculateTipButton = createCalculateTipButton()
     self.tipAmountLabel = createTipAmountLabel()
     self.backgroundColor = .white
@@ -41,6 +47,21 @@ class MainView: UIView {
     return it
   }
 
+  private func createTipPercentageLabel() -> UILabel {
+    let it = UILabel()
+    it.translatesAutoresizingMaskIntoConstraints = false
+    it.text = "Tip Percentage"
+    return it
+  }
+
+  private func createTipPercentageField() -> UITextField {
+    let it = UITextField()
+    it.translatesAutoresizingMaskIntoConstraints = false
+    it.placeholder = "Enter Tip Percentage"
+    it.text = String(defaultTipPercentage)
+    return it
+  }
+
   private func createCalculateTipButton() -> UIButton {
     let it = UIButton(type: .system)
     it.translatesAutoresizingMaskIntoConstraints = false
@@ -58,12 +79,15 @@ class MainView: UIView {
 
   @objc private func clickCalculateTipButton() {
     let amount = Double(billAmountField.text!)!
-    tipAmountLabel.text = String(amount + amount * 0.15)
+    let percentage = Double(tipPercentageField.text!)!
+    tipAmountLabel.text = String(amount + amount * percentage / 100)
   }
 
   private func addSubviews() {
     addSubview(billAmountLabel)
     addSubview(billAmountField)
+    addSubview(tipPercentageLabel)
+    addSubview(tipPercentageField)
     addSubview(calculateTipButton)
     addSubview(tipAmountLabel)
   }
@@ -76,8 +100,14 @@ class MainView: UIView {
       billAmountField.centerXAnchor.constraint(equalTo: centerXAnchor),
       billAmountField.topAnchor.constraint(equalTo: billAmountLabel.bottomAnchor, constant: 16),
 
+      tipPercentageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+      tipPercentageLabel.topAnchor.constraint(equalTo: billAmountField.bottomAnchor, constant: 16),
+
+      tipPercentageField.centerXAnchor.constraint(equalTo: centerXAnchor),
+      tipPercentageField.topAnchor.constraint(equalTo: tipPercentageLabel.bottomAnchor, constant: 16),
+
       calculateTipButton.centerXAnchor.constraint(equalTo: centerXAnchor),
-      calculateTipButton.topAnchor.constraint(equalTo: billAmountField.bottomAnchor, constant: 16),
+      calculateTipButton.topAnchor.constraint(equalTo: tipPercentageField.bottomAnchor, constant: 16),
 
       tipAmountLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
       tipAmountLabel.topAnchor.constraint(equalTo: calculateTipButton.bottomAnchor, constant: 16),
